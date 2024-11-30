b3b18fc4910f0342597b46deeb29a5748585f6e1
umtcnyldrm0 <umutcanyildirim04@gmail.com>
2024-11-30 19:40:54 +0300

    first commit

--- app/src/main/java/com/pulsetech/getirapp/MainActivity.java
+++ app/src/main/java/com/pulsetech/getirapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.pulsetech.getirapp;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
 
+
+    WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        webview =findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://www.google.com/maps");
+
+
     }
 }
